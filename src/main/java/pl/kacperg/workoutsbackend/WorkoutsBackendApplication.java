1b915257b479a0cac4b9120525ac1e707f8c7679
Kacper Gottel <kacper.gottel@appwise.pl>
2023-01-06 14:58:11 +0100

    init

---
+++ src/main/java/pl/kacperg/workoutsbackend/WorkoutsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.kacperg.workoutsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kacperg/workoutsbackend/WorkoutsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kacperg.workoutsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
