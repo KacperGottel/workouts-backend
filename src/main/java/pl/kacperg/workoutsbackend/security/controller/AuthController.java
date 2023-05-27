640c03383111d7b8fdafee1d79d5e48962a26fee
Kacper Gottel <kacper.gottel@appwise.pl>
2023-05-27 10:45:56 +0200

    token validation endpoint

--- src/main/java/pl/kacperg/workoutsbackend/security/controller/AuthController.java
+++ src/main/java/pl/kacperg/workoutsbackend/security/controller/AuthController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.kacperg.workoutsbackend.security.service.TokenService;
 
 @RestController
@@ -27,6 +24,11 @@ public class AuthController {
         TokenDTO tokenDTO = new TokenDTO();
         tokenDTO.setToken(token);
         return ResponseEntity.ok(tokenDTO);
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<Boolean> validateToken() {
+        return ResponseEntity.ok(true);
     }
 
     @Data
