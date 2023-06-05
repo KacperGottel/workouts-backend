3caaa91d8e29d0ef0279a5b8ac866e1c2bc311ad
Kacper Gottel <kacper.gottel@appwise.pl>
2023-06-05 22:37:00 +0200

    user get full impl

--- src/main/java/pl/kacperg/workoutsbackend/utils/parser/SortParser.java
+++ src/main/java/pl/kacperg/workoutsbackend/utils/parser/SortParser.java
@@ -1,14 +1,12 @@
 package pl.kacperg.workoutsbackend.utils.parser;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SortParser {
-
-    private SortParser() {
-        throw new IllegalStateException("Utility class");
-    }
 
     public static Sort parseSort(String sort) {
         String[] sortArr = sort.split(",");
